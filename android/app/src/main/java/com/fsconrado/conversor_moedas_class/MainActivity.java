195054e5c28adf5d6a257c2dd056c81e1a232ecd
Conrado <fabiano@nti.ufal.br>
2019-07-17 14:17:55 -0300

    Initial commit

---
+++ android/app/src/main/java/com/fsconrado/conversor_moedas_class/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fsconrado.conversor_moedas_class;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
